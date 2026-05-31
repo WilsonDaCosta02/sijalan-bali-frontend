0c22df97cc27b745a85cfc2a3e5f90fc3997cc8c
WilsonDaCosta02 <wilsondacosta0205@gmail.com>
2026-05-31 18:10:04 +0800

    style: remove debug logs from auto-scroll functionality in ReportHistory component

--- src/pages/user/ReportHistory.tsx
+++ src/pages/user/ReportHistory.tsx
@@ -132,12 +132,7 @@ const ReportHistory = () => {
   useEffect(() => {
     const shouldScroll = sessionStorage.getItem("scrollToLatestReport");
 
-    console.log("FLAG:", shouldScroll);
-    console.log("JUMLAH:", filteredReports.length);
-
     if (shouldScroll === "true" && filteredReports.length > 0) {
-      console.log("AUTO SCROLL JALAN");
-
       setTimeout(() => {
         lastReportRef.current?.scrollIntoView({
           behavior: "smooth",
